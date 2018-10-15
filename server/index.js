6989817f5ec68acb463de1dee9c09a853ca4e553
surtich <surtich@gmail.com>
2018-10-15 09:20:31 +0200

    mock schema implementation using faker

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import { ApolloServer, gql } from 'apollo-server';
 import { typeDefs } from './data/schema';
+import mockResolver from './data/mocks';
 
 const PORT = 8080;
 
-const server = new ApolloServer({ typeDefs, mocks: true });
+const server = new ApolloServer({ typeDefs, mocks: mockResolver });
 server.listen({ port: PORT }).then(({ url }) => console.log(`🚀 Server ready at ${url}`));
