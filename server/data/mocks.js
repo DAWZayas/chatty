ac1f042184fa97656b5dbc27d9e6e7aa9b3f9f3f
surtich <surtich@gmail.com>
2018-10-15 12:13:27 +0200

    Implementing DB resolvers and populated DB with mocked data

--- server/data/mocks.js
+++ server/data/mocks.js
@@ -1,30 +1,63 @@
+import R from 'ramda';
 import faker from 'faker';
+import { db } from './connectors';
 
-export const mocks = {
-  Date: () => new Date(),
-  Int: () => parseInt(Math.random() * 100, 10),
-  String: () => 'It works!',
-  Query: () => ({
-    user: (root, args) => ({
-      email: args.email,
-      messages: [
-        {
-          from: {
-            email: args.email,
-          },
-        },
-      ],
-    }),
-  }),
-  User: () => ({
-    email: faker.internet.email(),
-    username: faker.internet.userName(),
-  }),
-  Group: () => ({
-    name: faker.lorem.words(Math.random() * 3),
-  }),
-  Message: () => ({
-    text: faker.lorem.sentences(Math.random() * 3),
-  }),
+// create fake starter data
+const GROUPS = 4;
+const USERS_PER_GROUP = 5;
+const MESSAGES_PER_USER = 5;
+
+faker.seed(123); // get consistent data every time we reload app
+
+// you don't need to stare at this code too hard
+// just trust that it fakes a bunch of groups, users, and messages
+
+const mockDB = async () => {
+  console.log('creating database....');
+  await db.sync({ force: true });
+
+  console.log('populating groups....');
+  const groups = await Promise.all(
+    R.times(
+      () => db.models.group.create({
+        name: faker.lorem.words(3),
+      }),
+      GROUPS,
+    ),
+  );
+
+  console.log('populating users....');
+  const usersGroups = await Promise.all(
+    R.map(async (group) => {
+      const users = await Promise.all(
+        R.times(async () => {
+          const user = await group.createUser({
+            email: faker.internet.email(),
+            username: faker.internet.userName(),
+            password: faker.internet.password(),
+          });
+          R.times(
+            () => db.models.message.create({
+              userId: user.id,
+              groupId: group.id,
+              text: faker.lorem.sentences(3),
+            }),
+            MESSAGES_PER_USER,
+          );
+          return user;
+        }, USERS_PER_GROUP),
+      );
+      return users;
+    }, groups),
+  );
+
+  console.log('populating friends....');
+  await R.map(
+    users => users.map((current, i) => users.map((user, j) => (i !== j ? current.addFriend(user) : false))),
+    usersGroups,
+  );
+
+  console.log('¡DATABASE CREATED!');
 };
-export default mocks;
+
+export default mockDB;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
+import { resolvers } from './data/resolvers';
 import { typeDefs } from './data/schema';
-import mockResolver from './data/mocks';
+
+import mockDB from './data/mocks';
 
 const PORT = 8080;
 
-const server = new ApolloServer({ typeDefs, mocks: mockResolver });
-server.listen({ port: PORT }).then(({ url }) => console.log(`🚀 Server ready at ${url}`));
+const startServer = async () => {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await server.listen({ port: PORT });
+  console.log(`🚀 Server ready at ${url}`);
+};
+
+const init = async () => {
+  await mockDB();
+  startServer();
+};
+
+init();
