84aafe906338161b7ea5c65a5abe665162fc527a
surtich <surtich@gmail.com>
2018-10-17 09:17:42 +0200

    [mobile] Added faked groups screen

--- client/src/navigation.js
+++ client/src/navigation.js
@@ -12,6 +12,8 @@ import {
 import { Text, View, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 
+import Groups from './screens/groups.screen';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -28,7 +30,7 @@ const TestScreen = title => () => (
 // tabs in main screen
 const MainScreenNavigator = createMaterialTopTabNavigator(
   {
-    Chats: { screen: TestScreen('Chats') },
+    Chats: { screen: Groups },
     Settings: { screen: TestScreen('Settings') },
   },
   {
---
+++ client/src/screens/groups.screen.js
@@ -0,0 +1,67 @@
+import R from 'ramda';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import {
+  FlatList, StyleSheet, Text, TouchableHighlight, View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  groupContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderBottomColor: '#eee',
+    borderBottomWidth: 1,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+  },
+  groupName: {
+    fontWeight: 'bold',
+    flex: 0.7,
+  },
+});
+// create fake data to populate our ListView
+const fakeData = () => R.times(
+  i => ({
+    id: i,
+    name: `Group ${i}`,
+  }),
+  100,
+);
+const Group = ({ group: { id, name } }) => (
+  <TouchableHighlight key={id}>
+    <View style={styles.groupContainer}>
+      <Text style={styles.groupName}>{`${name}`}</Text>
+    </View>
+  </TouchableHighlight>
+);
+Group.propTypes = {
+  group: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+  }),
+};
+class Groups extends Component {
+  static navigationOptions = {
+    title: 'Chats',
+  };
+
+  keyExtractor = item => item.id.toString();
+
+  renderItem = ({ item }) => <Group group={item} />;
+
+  render() {
+    // render list of groups for user
+    return (
+      <View style={styles.container}>
+        <FlatList data={fakeData()} keyExtractor={this.keyExtractor} renderItem={this.renderItem} />
+      </View>
+    );
+  }
+}
+export default Groups;
