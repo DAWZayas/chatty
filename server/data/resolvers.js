721f6486115a666d4c6adfdb633db05a6aa75f72
Javier Pérez Arteaga <surtich@gmail.com>
2018-11-17 17:15:12 +0100

    [server] Added friend CRUD

--- server/data/resolvers.js
+++ server/data/resolvers.js
@@ -1,5 +1,7 @@
 import GraphQLDate from 'graphql-date';
-import { Group, Message, User } from './connectors';
+import {
+  BlackList, FriendInvitation, Group, Message, User,
+} from './connectors';
 
 export const resolvers = {
   Date: GraphQLDate,
@@ -15,6 +17,18 @@ export const resolvers = {
     },
     user(_, args) {
       return User.findOne({ where: args });
+    },
+    friendInvitations(_, { userId }) {
+      return FriendInvitation.findAll({
+        where: { $or: [{ fromId: userId }, { toId: userId }] },
+        order: [['createdAt']],
+      });
+    },
+    blackList(_, { userId }) {
+      return BlackList.findAll({
+        where: { $or: [{ fromId: userId }, { toId: userId }] },
+        order: [['createdAt']],
+      });
     },
   },
   Mutation: {
@@ -37,7 +51,9 @@ export const resolvers = {
       },
     ) {
       const user = await User.findOne({ where: { id: userId } });
-      const friends = await user.getFriends({ where: { id: { $in: userIds } } });
+      const friends = await user.getFriends({
+        where: { id: { $in: userIds } },
+      });
       const group = await Group.create({
         name,
         users: [user, ...friends],
@@ -69,6 +85,89 @@ export const resolvers = {
       },
     ) {
       return Group.findOne({ where: { id } }).then(group => group.update({ name }));
+    },
+    async createFriendInvitation(
+      _,
+      {
+        invitation: { text, from, to },
+      },
+    ) {
+      const user = await User.findOne({ where: { id: from } });
+      const friends = await user.getFriends({ where: { id: to } });
+
+      if (friends && friends.length) {
+        throw new Error(`user ${to} is already friend of ${from}`);
+      }
+
+      const previousInvitation = await FriendInvitation.findOne({
+        where: { $or: [{ fromId: from, toId: to }, { fromId: to, toId: from }] },
+      });
+
+      if (previousInvitation) {
+        throw new Error(
+          `There is already an invitation between ${to} and ${from}`,
+        );
+      }
+
+      const inBlackList = await BlackList.findOne({
+        where: { $or: [{ fromId: from, toId: to }, { fromId: to, toId: from }] },
+      });
+
+      if (inBlackList) {
+        throw new Error(
+          `Users ${to} and ${from} are in black list`,
+        );
+      }
+      
+      const invitation = await FriendInvitation.create({
+        fromId: from,
+        toId: to,
+        text,
+      });
+
+      return invitation;
+    },
+    cancelFriendInvitation(_, { from, to }) {
+      return FriendInvitation.destroy({ where: { fromId: from, toId: to } });
+    },
+    async acceptFriendInvitation(_, { from, to }) {
+      const fromUser = await User.findOne({ where: { id: from } });
+      const toUser = await User.findOne({ where: { id: to } });
+      const invitationExists = await FriendInvitation.destroy({
+        where: { fromId: from, toId: to },
+      });
+
+      if (invitationExists) {
+        await fromUser.addFriend(toUser);
+        await toUser.addFriend(fromUser);
+      }
+      return toUser;
+    },
+    async deleteFriend(_, { from, to }) {
+      const fromUser = await User.findOne({ where: { id: from } });
+      const toUser = await User.findOne({ where: { id: to } });
+      await fromUser.removeFriend(toUser);
+      await toUser.removeFriend(fromUser);
+      return fromUser;
+    },
+    async addToBlackList(_, { from, to }) {
+      const fromUser = await User.findOne({ where: { id: from } });
+      const toUser = await User.findOne({ where: { id: to } });
+      await fromUser.removeFriend(toUser);
+      await toUser.removeFriend(fromUser);
+      await FriendInvitation.destroy({
+        where: { fromId: from, toId: to },
+      });
+      await FriendInvitation.destroy({
+        where: { fromId: to, toId: from },
+      });
+      return BlackList.create({
+        fromId: from,
+        toId: to,
+      });
+    },
+    removeFromBlackList(_, { from, to }) {
+      return BlackList.destroy({ where: { fromId: from, toId: to } });
     },
   },
   Group: {
@@ -102,6 +201,22 @@ export const resolvers = {
     },
     friends(user) {
       return user.getFriends();
+    },
+  },
+  FriendInvitation: {
+    async from(invitation) {
+      return invitation.getFrom();
+    },
+    async to(invitation) {
+      return invitation.getTo();
+    },
+  },
+  BlackList: {
+    async from(blackList) {
+      return blackList.getFrom();
+    },
+    async to(blackList) {
+      return blackList.getTo();
     },
   },
 };
--- server/data/schema.js
+++ server/data/schema.js
@@ -27,6 +27,13 @@ export const typeDefs = gql`
     name: String
   }
 
+  # input for creating friend invitations
+  input CreateFriendInvitationInput {
+    from: Int! # user id of the user who sends invitation
+    to: Int! # user id for which the invitation is
+    text: String # invitation text
+  }
+
   # a group chat entity
   type Group {
     id: Int! # unique id for the group
@@ -51,6 +58,22 @@ export const typeDefs = gql`
     text: String! # message text
     createdAt: Date! # when message was created
   }
+
+  # a friend invitation send from one user to another
+  type FriendInvitation {
+    id: Int! # unique id for invitation
+    to: User! # user that has been invited
+    from: User! # user who sent the message
+    text: String! # invitation message text
+  }
+
+  # black list between two users
+  type BlackList {
+    id: Int! # unique id for black list
+    to: User! # user in the black list
+    from: User! # user who makes the action
+  }
+
   # query for types
   type Query {
     # Return a user by their email or id
@@ -60,14 +83,33 @@ export const typeDefs = gql`
     messages(groupId: Int, userId: Int): [Message]
     # Return a group by its id
     group(id: Int!): Group
+
+    # Return friend invitations from and to an user
+    friendInvitations(userId: Int!): [FriendInvitation]
+    # me black list
+    blackList(userId: Int!): [BlackList]
   }
   type Mutation {
     # send a message to a group
     createMessage(message: CreateMessageInput!): Message
+
+    # group CRUD
     createGroup(group: CreateGroupInput!): Group
     deleteGroup(id: Int!): Group
     leaveGroup(id: Int!, userId: Int!): Group # let user leave group
     updateGroup(group: UpdateGroupInput!): Group
+
+    # friend invitation CRUD
+    createFriendInvitation(
+      invitation: CreateFriendInvitationInput!
+    ): FriendInvitation
+    cancelFriendInvitation(from: Int!, to: Int!): Boolean
+    acceptFriendInvitation(from: Int!, to: Int!): User
+
+    deleteFriend(from: Int!, to: Int!): User
+
+    addToBlackList(from: Int!, to: Int!): BlackList
+    removeFromBlackList(from: Int!, to: Int!): Boolean
   }
   schema {
     query: Query
